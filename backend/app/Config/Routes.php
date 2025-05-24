79ee8859c35ec0e458e1a2d494828ff8ee2fc7a4
phcarapaz <smartnavyboy@gmail.com>
2025-05-24 15:55:43 +0700

    backend connect DB

--- backend/app/Config/Routes.php
+++ backend/app/Config/Routes.php
@@ -6,3 +6,13 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+$routes->resource('api/users', ['controller' => 'Api\Users']);
+$routes->options('api/(:segment)', 'Api\Users::options');
+$routes->options('api/(:any)', function () {
+    return service('response')
+        ->setHeader('Access-Control-Allow-Origin', '*')
+        ->setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+        ->setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+        ->setStatusCode(200);
+});
+
