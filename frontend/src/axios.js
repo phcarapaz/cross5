97a9da9b8420aa6ecdea9c812b5dce7fe36f67f4
phcarapaz <smartnavyboy@gmail.com>
2025-05-24 16:06:03 +0700

    Update axios.js

--- frontend/src/axios.js
+++ frontend/src/axios.js
@@ -3,6 +3,7 @@ import axios from 'axios'
 
 const apiClient = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL, 
+  baseURL: 'http://localhost:8080/api', 
   headers: {
     'Content-Type': 'application/json',
   },
